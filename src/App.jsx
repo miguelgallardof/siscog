e6cd1d4bc797927a25123f92a5aec7f30d832fc7
miguelgallardof <miguel.gallardof@icloud.com>
2023-10-08 15:00:44 -0500

    update

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+//import { useState } from 'react'
+import { Routes, Route } from "react-router-dom";
+import { Home, About } from "./pages"
+
+import { NavBar } from "./components/NavBar"
+
+function App() {
+  return (
+    <>    
+        <NavBar/>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          
+        </Routes>
+
+        
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/About/index.jsx
@@ -0,0 +1,7 @@
+export const About = () => {
+  return (
+    <>
+      <h1>Soy About</h1>
+    </>
+  )
+}
