904d010fb40a9e2b7224d4c9afbeb4fc79763412
miguelgallardof <miguel.gallardof@icloud.com>
2023-10-12 00:47:54 -0500

    update

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
 import { NavLink } from "react-router-dom";
 import { BurgerButton } from './BurgerButton'
-//import LogoNegative from '../assets/gales/logo-negative.svg'
-import LogoPositive from '../assets/gales/logo-positive.svg'
+import SiscogPositive from '../assets/siscog/logo-positive.svg'
+import SiscogNegative from '../assets/siscog/logo-negative.svg'
+import GalesNegative from '../assets/gales/logo-negative.svg'
 
 import styled from 'styled-components'
 
@@ -14,46 +15,62 @@ export const NavBar = () => {
   }
   return (
     <>
-      <NavContainer>
-        <h2>Navbar <span>Responsive</span></h2>
+    <NavContainer>
+    <Nav>
+        <img src={SiscogPositive} alt='siscog'/>
+        {/* <h2>Navbar <span>Responsive</span></h2> */}
         <div className={`links ${clicked ? 'active' : ''}`}>
           {/* <a className='link' onClick={handleClick} href="/">Home</a>
           <a className='link' onClick={handleClick} href="/About">About</a> */}
-          <NavLink to={"/"} >
-            <a onClick={handleClick} href="/">Home</a>
+          <NavLink to={"/"} onClick={handleClick}>
+            Inicio
           </NavLink>
-          <NavLink isActive='true' className='link' onClick={handleClick} to={"/about"}>
-            About
+          <NavLink to={"/about"} activeClassName='active'>
+            Nosotros
           </NavLink>
           <NavLink to={"/*"}>
-            Horror
+            Grupo Siscog
+          </NavLink>
+          <NavLink to={"/*"}>
+            Siscog Ingenieros 
+          </NavLink>
+          <NavLink to={"/*"}>
+            Contacto
           </NavLink>
         </div>
         <div className='burger'>
           <BurgerButton clicked={clicked} handleClick={handleClick} />
         </div>
-      </NavContainer>
+      </Nav>
       <Bg className={`initial ${clicked ? ' active' : ''}`}>
       {/* <h3>Desarrollado por <img src={LogoNegative} alt='gales logo'/></h3> */}
-      <h3>Desarrollado por <img src={LogoPositive} alt='gales logo'/></h3>
+      <h3>Desarrollado por <img src={GalesNegative} alt='gales logo'/></h3>
       </Bg>
       <Content className={`initial ${clicked ? ' active' : ''}`}>
-        <h1>Logo</h1>
+      <img src={SiscogNegative} alt='siscog'/>
       </Content>
+    </NavContainer>  
     </>
   )
 }
 
 const NavContainer = styled.nav`
+  position: fixed;
+  width: 100%;
+  height: 100%;
+`
+
+const Nav = styled.div`
   padding: 1rem 1.5rem;
   margin: 1rem;
   border-radius: 1rem;
   background-color: rgba(255, 255, 255, 0.5);
-  /* opacity: 0.85; */
   display: flex;
   align-items: center;
   justify-content: space-between;
   z-index: 100;
+
+
   h2{
     color: var(--main-color);
     font-weight: 400;
@@ -61,11 +78,18 @@ const NavContainer = styled.nav`
       font-weight: bold;
     }
   }
+
+  img {
+    height: 2.5rem;
+  }
+
   a{
     color: cyan;
     text-decoration: none;
     margin-left: 1rem;
+    display:none;
   }
+
   .links{
     position: absolute;
     top: -700px;
@@ -76,38 +100,55 @@ const NavContainer = styled.nav`
     text-align: center;
     transition: all .75s ease;
     z-index: 2;
+    
     a{ //Cuando salen
-      color: red;
+      color: cyan;
       font-size: 2rem;
       display: block;
     }
+
     @media(min-width: 768px){
       position: initial;
       margin: 0;
       a{
         font-size: 1rem;
-        color: var(--main-color);
+        color: red;
         display: inline;
       }
       display: block;
     }
   }
+
   .links.active{
     width: 100%;
     display: block;
     position: absolute;
     margin-left: auto;
     margin-right: auto;
-    top: 30%;
+    top: 20%;
     left: 0;
     right: 0;
     text-align: center;
     a{
       font-size: 2rem;
       margin-top: 1rem;
-      color: yellow;
+      color: var(--main-color)
     }
+    /* @media(min-width: 768px){
+      position: initial;
+      margin: 0;
+      a{
+        font-size: 1rem;
+        color: red;
+        display: inline;
+      }      
+    } */
   }
+
+  .links-mobile {
+    display: none;
+  }
+
   .burger{
     z-index: 2;
     @media(min-width: 768px){
@@ -123,6 +164,7 @@ const Bg = styled.div`
   left: -1000px;
   z-index: 1;
   transition: all .25s ease ;
+  display:block;
   
   &.active{
     top: 0;
@@ -140,6 +182,10 @@ const Bg = styled.div`
     color: white;
     opacity: 0.25;
   }
+
+  /* @media(min-width: 768px){
+    display: none;
+  } */
 `
 const Content = styled.div`
   background-color: var(--second-color);
@@ -149,7 +195,7 @@ const Content = styled.div`
   width: 100%;
   height: 100%;
   z-index: 1;
-  transition: all .75s ease ;
+  transition: all .75s ease;
   
   &.active{
     border-radius: 0 0 100% 0;
@@ -157,5 +203,14 @@ const Content = styled.div`
     left: 0;
     width: 100%;
     height: 100%;
+  }
+
+  /* @media(min-width: 768px){
+    display: none;
+  } */
+
+  img {
+    height: 2.5rem;
+    margin: 2rem 2.5rem
   }
 `
